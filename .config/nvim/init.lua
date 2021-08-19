ef05b356d87a93e4bdfccc2e50ee50c8ff8d2380
160R <160R@protonmail.com>
2021-08-19 22:36:47 +0200

    moved utocmds to lua; deleted _init.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -75,5 +75,25 @@ opt.whichwrap = 'b,s,<,>,h,l,[,]'
 opt.splitbelow = true
 opt.splitright = true
 
+vim.cmd [[
+au! VimEnter * :silent exec "!kill -s SIGWINCH $PPID"
+au! CursorHold * checktime
 
-exec 'source ~/.config/nvim/_init.vim'
+au BufEnter * if empty(&ft) && &buftype != 'terminal' | set filetype=markdown | endif
+
+function! PageClose(page_alternate_bufnr)
+bd!
+if bufnr('%') == a:page_alternate_bufnr && mode('%') == 'n'
+  norm a
+endif
+endfunction
+au! User PageOpen exe 'map <buffer> <C-c> :call PageClose(b:page_alternate_bufnr)<CR>' | exe 'tmap <bufer> <C-c> :call PageClose(b:page_alternate_bufnr)<CR>'
+
+au! User GoyoLeave hi Normal guibg=NONE ctermbg=NONE
+
+aug VMlens
+  au!
+  au User visual_multi_start lua require('vmlens').start()
+  au User visual_multi_exit lua require('vmlens').exit()
+aug END]]
+
