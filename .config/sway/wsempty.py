27b840057ffaecb29790ababbf5910cfe1dca839
160R <160R@protonmail.com>
2022-08-19 15:35:16 +0200

    up

---
+++ .config/sway/wsempty.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Spawn GUI program on an empty workspace
+
+import sys, subprocess
+
+from i3ipc import Connection
+
+
+sway = Connection()
+
+
+def execute_command(ws_number: int):
+    sway.command('workspace number %d' % ws_number)
+    subprocess.Popen(sys.argv[1:])
+
+
+def main():
+    i = 1
+    for ws in sway.get_tree().workspaces():
+        if not ws.leaves():
+            execute_command(i)
+        if ws.num == i:
+            i += 1
+            continue
+    execute_command(i)
+
+if __name__ == "__main__":
+    main()
