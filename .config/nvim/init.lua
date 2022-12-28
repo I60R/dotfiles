6dcb8400306bfd4caa6419666112eb40b4b9a130
160R <160R@protonmail.com>
2022-12-28 01:46:18 +0100

    neovide transparency

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.no_plugin_maps = true
 
 if vim.g.neovide then
     vim.o.guifont = 'JetBrains Mono:h13'
-    vim.g.neovide_transparency = 0.0
+    vim.g.neovide_transparency = 0.4
     vim.g.neovide_floating_blur_amount_x = 2.0
     vim.g.neovide_floating_blur_amount_y = 2.0
     vim.g.neovide_confirm_quit = false
